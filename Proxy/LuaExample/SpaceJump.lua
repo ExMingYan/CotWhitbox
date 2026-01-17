1a228e5bf8660744a7e67d40b886247c26cfad70
ExMingYan <2264568487@qq.com>
2026-01-17 10:55:16 +0800

    Add SpaceJump.lua for example

---
+++ Proxy/LuaExample/SpaceJump.lua
@@ -0,0 +1,48 @@
+-- Fatal Fury COTW Space Jump by ExMingYan
+
+local ESCPatch = function(addr, offset)
+    addr[offset] = 0x06
+end
+
+local ArcadeESCMenu = function() -- 街机模式/Arcade
+    local ESCAddress = AOBScanModule("CotW-Win64-Shipping.exe", ".text", "EB ?? 41 80 ?? 0F 75 ?? 48 8B CB")
+    ESCPatch(ESCAddress, 5)
+end
+
+local PracticeESCMenu = function() -- 练习模式/Training
+    local ESCAddress = AOBScanModule("CotW-Win64-Shipping.exe", ".text", "EB ?? 41 80 ?? 0F 75 ?? 41 8B")
+    ESCPatch(ESCAddress, 5)
+end
+
+local SurvivalESCMenu = function() -- 生存模式/Survival | EOST
+    local ESCAddress = AOBScanModule("CotW-Win64-Shipping.exe", ".text", "E9 ?? ?? ?? ?? 41 80 ?? 0F 75 ?? 48 8B")
+    ESCPatch(ESCAddress, 8)
+end
+
+local TrialESCMenu = function() -- 考验任务/Trial | 竞速模式/Time Attack
+    local ESCAddress = AOBScanModule("CotW-Win64-Shipping.exe", ".text", "0F 85 ?? ?? ?? ?? 41 80 ?? 0F 75 ??")
+    ESCPatch(ESCAddress, 9)
+end
+
+local SetSpace = function() -- 按键设置可用Space键/Button Config can use Space Key
+    local SpaceAddress = AOBScanModule("CotW-Win64-Shipping.exe", ".text", "48 B9 FE 38 C0 FF CF 1E")
+    SpaceAddress[7] = 0x1A
+end
+
+return {
+    {
+        main = ArcadeESCMenu
+    },
+    {
+        main = PracticeESCMenu
+    },
+    {
+        main = SurvivalESCMenu
+    },
+    {
+        main = TrialESCMenu
+    },
+    {
+        main = SetSpace
+    }
+}
